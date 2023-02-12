e680f323e0cba3a522954b51ffa320a2b38a67c6
qwe916 <qwe916@naver.com>
2023-02-12 16:41:35 +0900

    generate spring project

---
+++ src/main/java/com/gdsc/bakku/BakkuApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.bakku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakkuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakkuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdsc/bakku/BakkuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.bakku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakkuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
