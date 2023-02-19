de71b240a2484ced3a6e1d48cc04dbd5f4392d86
qwe916 <qwe916@naver.com>
2023-02-20 02:42:51 +0900

    feat: Add function to find nearby ocean

--- src/main/java/com/gdsc/bakku/ocean/controller/OceanController.java
+++ src/main/java/com/gdsc/bakku/ocean/controller/OceanController.java
@@ -3,13 +3,11 @@ import com.gdsc.bakku.ocean.domain.entity.Ocean;
 import com.gdsc.bakku.ocean.service.OceanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -19,8 +17,10 @@ public class OceanController {
     private final OceanService oceanService;
 
     @GetMapping("/oceans")
-    public ResponseEntity<Slice<Ocean>> findAll(@PageableDefault(size = 5) Pageable pageable) {
-        Slice<Ocean> oceans = oceanService.findAll(pageable);
+    public ResponseEntity<List<Ocean>> findAll(@RequestParam(name = "lat") Double latitude,
+                                               @RequestParam(name = "lon") Double longitude,
+                                               @PageableDefault(size = 5) Pageable pageable) {
+        List<Ocean> oceans = oceanService.findAll(latitude,longitude,pageable);
 
         if (oceans.isEmpty()) {
             return ResponseEntity.noContent().build();
--- src/main/java/com/gdsc/bakku/ocean/service/OceanService.java
+++ src/main/java/com/gdsc/bakku/ocean/service/OceanService.java
@@ -1,24 +1,30 @@
 package com.gdsc.bakku.ocean.service;
 
+import com.gdsc.bakku.common.entity.Location;
 import com.gdsc.bakku.ocean.domain.entity.Ocean;
 import com.gdsc.bakku.ocean.domain.repo.OceanRepository;
+import com.gdsc.bakku.ocean.domain.repo.OceanRepositorySupport;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OceanService {
 
     private final OceanRepository oceanRepository;
+    private final OceanRepositorySupport oceanRepositorySupport;
 
     @Transactional(readOnly = true)
-    public Slice<Ocean> findAll(Pageable pageable) {
-        return oceanRepository.findAllBy(pageable);
+    public List<Ocean> findAll(Double latitude, Double longitude, Pageable pageable) {
+        Location location = new Location(longitude, latitude);
+
+        return oceanRepositorySupport.findCloseOcean(location, pageable);
     }
 
     @Transactional(readOnly = true)
