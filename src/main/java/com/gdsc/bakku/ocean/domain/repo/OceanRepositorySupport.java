7e7685ad3681ddc8c0b6608d3d57d5d7ff18a8d9
HanHyunSoo <gustn4563@gmail.com>
2023-02-28 14:56:49 +0900

    fix: Fix Query(findCloseOcean)
    null을 뒤로 보내는 쿼리를 작성

--- src/main/java/com/gdsc/bakku/ocean/domain/repo/OceanRepositorySupport.java
+++ src/main/java/com/gdsc/bakku/ocean/domain/repo/OceanRepositorySupport.java
@@ -3,6 +3,7 @@ package com.gdsc.bakku.ocean.domain.repo;
 import com.gdsc.bakku.ocean.domain.entity.Ocean;
 import com.gdsc.bakku.ocean.domain.entity.QOcean;
 import com.gdsc.bakku.ocean.dto.OceanDTO;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -25,13 +26,24 @@ public class OceanRepositorySupport {
         List<Ocean> oceanList = jpaQueryFactory.selectFrom(ocean)
                 .limit(pageable.getPageSize() + 1)
                 .offset(pageable.getOffset())
-                .orderBy(ocean.position.latitude.asc().nullsLast(), ocean.position.longitude.asc().nullsLast(),
+                .orderBy(
+                        new CaseBuilder()
+                                .when(ocean.position.latitude.isNull())
+                                .then(1)
+                                .otherwise(0)
+                                .asc(),
+                        new CaseBuilder()
+                                .when(ocean.position.longitude.isNull())
+                                .then(1)
+                                .otherwise(0)
+                                .asc(),
                         Expressions.stringTemplate("ST_Distance_Sphere({0}, {1})",
                                 Expressions.stringTemplate("POINT({0}, {1})",
                                         longitude, latitude),
                                 Expressions.stringTemplate("POINT({0}, {1})",
                                         ocean.position.longitude, ocean.position.latitude))
-                        .asc())
+                        .asc()
+                )
                 .fetch();
 
         List<OceanDTO> oceanDTOS = new ArrayList<>();
