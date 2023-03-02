b5668e218e0b383c053664f317f76b3da8fff8d1
HanHyunSoo <gustn4563@gmail.com>
2023-03-02 17:32:33 +0900

    refactor: Edit Exception Response for Authentication and Authorization

--- src/main/java/com/gdsc/bakku/auth/handler/CustomAccessDeniedHandler.java
+++ src/main/java/com/gdsc/bakku/auth/handler/CustomAccessDeniedHandler.java
@@ -3,6 +3,7 @@ package com.gdsc.bakku.auth.handler;
 import com.gdsc.bakku.auth.util.ResponseUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
@@ -11,6 +12,6 @@ import java.io.IOException;
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
-        ResponseUtil.setResponse(response, 403, "NO_PERMISSION");
+        ResponseUtil.setResponse(response, HttpStatus.FORBIDDEN, "NO_PERMISSION");
     }
 }
