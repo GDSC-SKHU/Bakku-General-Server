c28140e14b6973585a3d717958fc03708bf1df6b
HanHyunSoo <gustn4563@gmail.com>
2023-03-08 15:19:58 +0900

    docs: Add Descriptions and Examples to DTOs

--- src/main/java/com/gdsc/bakku/bakku/dto/request/BakkuImageRequest.java
+++ src/main/java/com/gdsc/bakku/bakku/dto/request/BakkuImageRequest.java
@@ -1,26 +1,30 @@
 package com.gdsc.bakku.bakku.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
+@Schema(description = "multipart/form-data 전용 이미지들 바꾸 request body입니다.(필드 제외)")
 public class BakkuImageRequest {
+    @Schema(description = "메인 이미지", implementation = Object.class)
     private MultipartFile titleImage;
 
+    @Schema(description = "청소 후 이미지")
     private MultipartFile afterImage;
 
+    @Schema(description = "청소 전 이미지")
     private MultipartFile beforeImage;
 
     @NotNull
+    @Schema(description = "메인 이미지를 바꾸는지에 대한 여부")
     private Boolean isChangeTitle;
 
     @NotNull
+    @Schema(description = "청소 후 이미지 바꾸는지에 대한 여부")
     private Boolean isChangeAfter;
 
     @NotNull
+    @Schema(description = "청소 전 이미지 바꾸는지에 대한 여부")
     private Boolean isChangeBefore;
 }
--- src/main/java/com/gdsc/bakku/event/dto/EventDTO.java
+++ src/main/java/com/gdsc/bakku/event/dto/EventDTO.java
@@ -1,20 +1,32 @@
 package com.gdsc.bakku.event.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 
 @Builder
+@Schema(description = "행사 응답")
 public record EventDTO(
+        @Schema(description = "PK", example = "1")
         Long id,
+        @Schema(description = "이름", example = "행사 이름")
         String name,
+        @Schema(description = "행사 시작 시간", example = "2023-02-27T20:44:28")
         LocalDateTime startDate,
+        @Schema(description = "행사 종료 시간", example = "2023-02-28T20:44:28")
         LocalDateTime endDate,
+        @Schema(description = "내용", example = "행사 내용")
         String comment,
+        @Schema(description = "위도", example = "37.58122552547526")
         Double latitude,
+        @Schema(description = "경도", example = "126.93754073973317")
         Double longitude,
+        @Schema(description = "이미지 url", example = "https://blog.kakaocdn.net/dn/3ghE9/btq2RjJq0zF/skhqeIUWQca9a8VDiPW1Sk/img.png")
         String imageUrl,
+        @Schema(description = "생성 시간", example = "2023-02-27T20:44:28")
         LocalDateTime createdDate,
+        @Schema(description = "수정 시간", example = "2023-02-27T20:44:28")
         LocalDateTime modifiedDate
 ) {
 }
