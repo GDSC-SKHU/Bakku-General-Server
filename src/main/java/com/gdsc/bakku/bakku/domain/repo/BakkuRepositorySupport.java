c7a6fec432619cb8c82d87d3551e7b67900a12e7
HanHyunSoo <gustn4563@gmail.com>
2023-03-14 10:33:09 +0900

    fix: Fix Ordering in Bakku Read

--- src/main/java/com/gdsc/bakku/bakku/domain/repo/BakkuRepositorySupport.java
+++ src/main/java/com/gdsc/bakku/bakku/domain/repo/BakkuRepositorySupport.java
@@ -5,14 +5,18 @@ import com.gdsc.bakku.bakku.domain.entity.Bakku;
 import com.gdsc.bakku.bakku.domain.entity.QBakku;
 import com.gdsc.bakku.group.domain.entity.Group;
 import com.gdsc.bakku.ocean.domain.entity.Ocean;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -27,7 +31,7 @@ public class BakkuRepositorySupport {
                 .where(getBooleanExpressions(group, ocean, user))
                 .limit(pageable.getPageSize() + 1)
                 .offset(pageable.getOffset())
-                .orderBy(bakku.id.desc())
+                .orderBy(getOrderSpecifiers(pageable.getSort()))
                 .fetch();
 
         boolean hasNext = false;
@@ -38,6 +42,32 @@ public class BakkuRepositorySupport {
         }
 
         return new SliceImpl<>(bakkuList, pageable, hasNext);
+    }
+
+    private OrderSpecifier<?>[] getOrderSpecifiers(Sort sort) {
+        List<OrderSpecifier<?>> orderSpecifiers = new ArrayList<>();
+
+        for (Sort.Order order : sort) {
+            Order direction = order.getDirection().isAscending() ? Order.ASC : Order.DESC;
+            OrderSpecifier<?> orderSpecifier = null;
+            switch (order.getProperty()) {
+                case "id" -> orderSpecifier = new OrderSpecifier<>(direction, bakku.id);
+                case "cleanWeight" -> orderSpecifier = new OrderSpecifier<>(direction, bakku.cleanWeight);
+                case "decorateTime" -> orderSpecifier = new OrderSpecifier<>(direction, bakku.decorateDate);
+                case "createdDate" -> orderSpecifier = new OrderSpecifier<>(direction, bakku.createdDate);
+                case "modifiedDate" -> orderSpecifier = new OrderSpecifier<>(direction, bakku.modifiedDate);
+            }
+
+            if (orderSpecifier != null) {
+                orderSpecifiers.add(orderSpecifier);
+            }
+        }
+
+        if (!orderSpecifiers.isEmpty()) {
+            return orderSpecifiers.toArray(new OrderSpecifier<?>[0]);
+        } else {
+            return new OrderSpecifier<?>[0];
+        }
     }
 
     private BooleanExpression[] getBooleanExpressions(Group group, Ocean ocean, User user) {
