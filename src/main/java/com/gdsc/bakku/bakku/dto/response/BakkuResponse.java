a4f3cbfa8c42e89c5bd7e0478cde3ecb06e1c314
HanHyunSoo <gustn4563@gmail.com>
2023-03-14 09:52:42 +0900

    docs: Fix example decorateTime Field

--- src/main/java/com/gdsc/bakku/bakku/dto/response/BakkuResponse.java
+++ src/main/java/com/gdsc/bakku/bakku/dto/response/BakkuResponse.java
@@ -14,7 +14,7 @@ public record BakkuResponse(
         String comment,
         @Schema(description = "청소한 무게(kg)", example = "10")
         int cleanWeight,
-        @Schema(description = "청소한 시간", example = "2023-02-27T20:44:28")
+        @Schema(description = "청소한 시간", example = "2023-02-27")
         LocalDate decorateTime,
         @Schema(description = "메인 이미지 url", example = "https://i.namu.wiki/i/TwwcGwWqzN8HWluj-kUbX4rzuhHristP1LsE3K_gG_vUfMoFiqPRT0_Nm5LPjySVzrvh5NsyDxVzV9sRpFec5iNswOqUjEYt2qmcGn7xtM44DwD_A28ma3iJD69Hnr4Ctfgcu6C7tN5Vo4-voblIWg.webp")
         String titleImageUrl,
